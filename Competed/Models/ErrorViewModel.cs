4e5186ce69181579e864b7708716b87be9a8d0fa
Brandon Clapp <brandonclapp@outlook.com>
2018-03-06 22:18:44 -0600

    Add project files.

---
+++ Competed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Competed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
