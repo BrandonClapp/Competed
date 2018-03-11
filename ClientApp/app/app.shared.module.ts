b5c5050e3727d807315e66360a507c6f8a1e647a
Brandon Clapp <brandonclapp@outlook.com>
2018-03-10 18:43:07 -0600

    home brainstorm

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
         NavMenuComponent,
         FetchDataComponent,
     ],
@@ -19,7 +21,7 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', component: FetchDataComponent, pathMatch: 'full' },
+            { path: '', component: HomeComponent, pathMatch: 'full' },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'fetch-data' }
         ])
