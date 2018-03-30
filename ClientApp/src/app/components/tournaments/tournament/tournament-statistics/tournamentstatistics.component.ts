cc303b8175aeb80b777d0863012a97787ad9b2e9
Brandon Clapp <brandonclapp@outlook.com>
2018-03-30 10:14:35 -0500

    Add additional tab components

---
+++ ClientApp/src/app/components/tournaments/tournament/tournament-statistics/tournamentstatistics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tournament-statistics',
+  templateUrl: './tournamentstatistics.component.html',
+  styleUrls: ['./tournamentstatistics.component.css']
+})
+export class TournamentStatisticsComponent {
+
+
+}
